494a500cf90cb539749898b4d2f8cb574bb0f69c
Shao-An Yin <drrdrem@gmail.com>
2019-04-20 12:40:44 -0700

    Add files via upload

--- design_by_induction.py
+++ design_by_induction.py
@@ -16,6 +16,27 @@
     return S
 
 
+# HW02P2; LeetCode#53
+# This is wrong
+# type nums: List[int]
+# rtype: int
+def maxSumSubsequence2(nums):
+    Sum = largest = smallest = 0
+    for i in range(len(nums)):
+        Sum = nums[i] + Sum                     # sum[i]
+        smallest = min(smallest, Sum)           # min{0<=j<=i} sum[j]
+        largest = max(largest, Sum-smallest)    # max{k<=i} sum[k] - min{0<=j<=k} sum[j]
+    return largest
+
+# Similar Approach but correct
+def maxSumSubsequence3(nums):
+    Sum = res = nums[0]
+    for num in nums[1:]:
+        Sum = max(num, Sum + num)
+        res = max(res, Sum)
+    return res
+
+
 # HW02P3; LeetCode#152
 # The solution is wrong
 # type nums: List[int]
@@ -27,7 +48,6 @@ def maxProduct(nums):
     for i in range(len(nums)):
         if nums[i]==0: ps = ns = 1
         else: 
-            ps, ns = max(ps*nums[i], ns*nums[i], nums[i]), min(ps*nums[i], ns*nums[i], nums[i])
-            
+            ps, ns = max(ps*nums[i], ns*nums[i], nums[i]), min(ps*nums[i], ns*nums[i], nums[i])           
             o = max(o, ps)          
     return o
