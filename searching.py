f25be69ccfd648030305205f4f8ca85acec6dc7a
Shao-An Yin <drrdrem@gmail.com>
2019-07-11 13:36:04 -0700

    Add files via upload

--- searching.py
+++ searching.py
@@ -2,20 +2,20 @@
 # type root: TreeNode
 # rtype: List[List[int]]
 def BFS(root):
-	if not root: return 
+    if not root: return 
 
-	queue, res = [root], []
-	while queue:
-		level_vertices = []
-		for _ in range(len(queue)):
-			u = queue.pop(0)
-			if u.left:
-				queue.append(u.left)
-			if u.right:
-				queue.append(u.right)
-			level_vertices.append(u.val)
-		res.append(level_vertices)
-	return res
+    queue, res = [root], []
+    while queue:
+        level_vertices = []
+        for _ in range(len(queue)):
+            u = queue.pop(0)
+            if u.left:
+                queue.append(u.left)
+            if u.right:
+                queue.append(u.right)
+            level_vertices.append(u.val)
+        res.append(level_vertices)
+    return res
 
 
 # Lecture#8; LeetCode#106
@@ -24,39 +24,39 @@ def BFS(root):
 # rtype: bool
 def DFS(root, sum):
     if not root: return
-	
-	stack = [(root, root.val)]
-	while stack:
-		vertex, accumu_val = stack.pop()
-		if not vertex.left and not vertex.right:
-			if accumu_val == sum: return True
-		if vertex.right:
-			stack.append((vertex.right, accumu_val+vertex.right.val))
-		if vertex.left:
-			stack.append((vertex.left, accumu_val+vertex.left.val))
-	return False
+    
+    stack = [(root, root.val)]
+    while stack:
+        vertex, accumu_val = stack.pop()
+        if not vertex.left and not vertex.right:
+            if accumu_val == sum: return True
+        if vertex.right:
+            stack.append((vertex.right, accumu_val+vertex.right.val))
+        if vertex.left:
+            stack.append((vertex.left, accumu_val+vertex.left.val))
+    return False
 
 
 # Lecture#7; LeetCode#785
 # type graph: List[List[int]]
 # rtype: bool
 def isBipartite(self, graph):
-	visit = [False]*len(graph)
-	q = [0];
-	while q:
-		level = []
-		for _ in range(len(q)):
-			node = q.pop(0)
-			level.append(node)
-			visit[node] = True
-			for n in graph[node]:
-				if not visit[n]:                    
-					q.append(n)
-				
-		for node in level:
-			for n in graph[node]:
-				if n in level: return False
-	return True
+    visit = [False]*len(graph)
+    q = [0];
+    while q:
+        level = []
+        for _ in range(len(q)):
+            node = q.pop(0)
+            level.append(node)
+            visit[node] = True
+            for n in graph[node]:
+                if not visit[n]:                    
+                    q.append(n)
+                
+        for node in level:
+            for n in graph[node]:
+                if n in level: return False
+    return True
 
 
 
